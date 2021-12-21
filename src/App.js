5be0fc2b2b4d243d3171517675b4710b776d6cc6
chauhanjaya4 <chauhanjaya01@gmail.com>
2021-12-21 22:00:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+
+const App = ()=>{
+  return<Navbar/>;
+  
+  
+};
+/*const MyName=()=>{
+  return(<><h1>jaya</h1></>);
+}*/
+export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,439 @@
+import React from 'react';
+
+
+import "../assets/css/style.css";
+
+
+
+
+
+
+const Navbar = () => {
+    return (
+        <>
+                  
+        <header className="main_menu">
+        <div className="container">
+        <div className="row align-items-center">
+        <div className="col-lg-12">
+        <nav className="navbar navbar-expand-lg navbar-light">
+        <a className="navbar-brand" href="index.html">
+             <img src="img/logo.png" alt="logo"/> </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse main-menu-item justify-content-center" id="navbarSupportedContent">
+        <ul className="navbar-nav">
+        <li className="nav-item active">
+        <a className="nav-link" href="index-2.html">Home</a>
+        </li>
+        <li className="nav-item active">
+        <a className="nav-link" href="index-2.html">About This Channel</a>
+        </li>
+        <li className="nav-item">
+        <a className="nav-link" href="archive.html">My Posts</a>
+        </li>
+        <li className="nav-item">
+        <a className="nav-link" href="category.html"> What's New</a>
+        </li>
+        <li className="nav-item">
+        <a className="nav-link" href="contact.html">Reach Me</a>
+        </li>
+        <li className="nav-item">
+        <a className="nav-link" href="contact.html">Weekly Updates</a>
+        </li>
+        <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Pages
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a className="dropdown-item" href="single-blog.html">Single blog</a>
+        <a className="dropdown-item" href="elements.html">elements</a>
+        </div>
+        </li>
+        </ul>
+        </div>
+        <div className="header_social_icon d-none d-lg-block">
+        <ul>
+        <li>
+        <div id="wrap">
+        
+        </div>
+        </li>
+        <li><a href="#" className="d-none d-lg-block"><i className="ti-facebook"></i></a></li>
+        <li><a href="#" className="d-none d-lg-block"> <i className="ti-twitter-alt"></i></a></li>
+        <li><a href="#" className="d-none d-lg-block"><i className="ti-instagram"></i></a></li>
+        <li><a href="#" className="d-none d-lg-block"><i className="ti-skype"></i></a></li>
+        </ul>
+        </div>
+        </nav>
+        </div>
+        </div>
+        </div>
+        </header> 
+        
+<section className="banner_post">
+<div className="container-fluid">
+<div className="row justify-content-between">
+<div className="banner_post_1 banner_post_bg_1">
+<div className="banner_post_iner text-center">
+<a href="category.html"><h5> Fashion / Lifestyle Hacks</h5></a>
+<a href="single-blog.html"><h2>Fashion And Lifestyle Ideas </h2></a>
+<p>Posted on April 15, 2019</p>
+</div>
+</div><div className="banner_post_1 banner_post_bg_2">
+<div className="banner_post_iner text-center">
+<a href="category.html"><h5> Daily Life Hacks And Ideas</h5></a>
+<a href="single-blog.html"><h2>Solution For Daily  Household Problems </h2></a>
+<p>Posted on April 15, 2019</p>
+</div>
+</div>
+<div className="banner_post_1 banner_post_bg_3">
+<div className="banner_post_iner text-center">
+<a href="category.html"><h5> Daily Utility Hacks</h5></a>
+<a href="single-blog.html"><h2>Daily Utility Stuffs Ideas And Hacks </h2></a>
+<p>Posted on April 15, 2019</p>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+
+<section className="all_post section_padding">
+<div className="container">
+<div className="row">
+<div className="col-lg-8">
+<div className="row">
+<div className="col-lg-12">
+<div className="single_post post_1 feature_post">
+<div className="single_post_img">
+<img src="img/post/fashion_tips.jpg" alt=""/>
+</div>
+<div className="single_post_text text-center">
+<a href="category.html"><h5>Shopping On Sale</h5></a>
+<a href="single-blog.html"><h2>Things To keep In Mind While purchasing Clothes On Sale </h2></a>
+<p>Posted on April 15, 2019</p>
+</div>
+</div>
+</div>
+<div className="col-lg-6 col-sm-6">
+<div className="single_post post_1">
+<div className="single_post_img">
+<img src="img/post/cleaning_tips.jpg" alt=""/>
+</div>
+<div className="single_post_text text-center">
+<a href="category.html"><h5> Best Cleaning Tips</h5></a>
+<a href="single-blog.html"><h2>Get Best Household Cleaning Tips</h2></a>
+<p>Posted on April 15, 2019</p>
+</div>
+</div>
+</div>
+<div className="col-lg-6 col-sm-6">
+<div className="single_post post_1">
+<div className="single_post_img">
+<img src="img/post/smelly_shoes.jpg" alt=""/>
+</div>
+<div className="single_post_text text-center">
+<a href="category.html"><h5> How To Get Rid Of Smelly Shoes</h5></a>
+<a href="single-blog.html"><h2>Tips For Getting Rid Of Smelly Shoes</h2></a>
+<p>Posted on April 15, 2019</p>
+</div>
+</div>
+</div>
+<div className="col-lg-6 col-sm-6">
+<div className="single_post post_1">
+<div className="single_post_img">
+<img src="img/post/post_20.png" alt=""/>
+</div>
+<div className="single_post_text text-center">
+<a href="category.html"><h5> Fashion / Life style</h5></a>
+<a href="single-blog.html"><h2>Your life is a product your
+right decisions</h2></a>
+<p>Posted on April 15, 2019</p>
+</div>
+</div>
+</div>
+<div className="col-lg-6 col-sm-6">
+<div className="single_post post_1">
+<div className="single_post_img">
+<img src="img/post/post_21.png" alt=""/>
+</div>
+<div className="single_post_text text-center">
+<a href="category.html"><h5> Fashion / Life style</h5></a>
+<a href="single-blog.html"><h2>Your life is a product your
+right decisions</h2></a>
+<p>Posted on April 15, 2019</p>
+</div>
+</div>
+</div>
+<div className="col-lg-6 col-sm-6">
+<div className="single_post post_1">
+<div className="single_post_img">
+<img src="img/post/post_22.png" alt=""/>
+</div>
+<div className="single_post_text text-center">
+<a href="category.html"><h5> Fashion / Life style</h5></a>
+<a href="single-blog.html"><h2>Your life is a product your
+right decisions</h2></a>
+<p>Posted on April 15, 2019</p>
+</div>
+</div>
+</div>
+<div className="col-lg-6 col-sm-6">
+<div className="single_post post_1">
+<div className="single_post_img">
+<img src="img/post/post_23.png" alt=""/>
+</div>
+<div className="single_post_text text-center">
+<a href="category.html"><h5> Fashion / Life style</h5></a>
+<a href="single-blog.html"><h2>Your life is a product your
+right decisions</h2></a>
+<p>Posted on April 15, 2019</p>
+</div>
+</div>
+</div>
+<div className="col-lg-6 col-sm-6">
+<div className="single_post post_1">
+<div className="single_post_img">
+<img src="img/post/post_24.png" alt=""/>
+</div>
+<div className="single_post_text text-center">
+<a href="category.html"><h5> Fashion / Life style</h5></a>
+<a href="single-blog.html"><h2>Your life is a product your
+right decisions</h2></a>
+<p>Posted on April 15, 2019</p>
+</div>
+</div>
+</div>
+<div className="col-lg-6 col-sm-6">
+<div className="single_post post_1">
+<div className="single_post_img">
+<img src="img/post/post_25.png" alt=""/>
+</div>
+<div className="single_post_text text-center">
+<a href="category.html"><h5> Fashion / Life style</h5></a>
+<a href="single-blog.html"><h2>Your life is a product your
+right decisions</h2></a>
+<p>Posted on April 15, 2019</p>
+</div>
+</div>
+</div>
+</div>
+<div className="page_pageniation">
+<nav aria-label="Page navigation example">
+<ul className="pagination justify-content-center">
+<li className="page-item disabled">
+<a className="page-link" href="#" tabIndex="-1" aria-disabled="true">Previous</a>
+</li>
+<li className="page-item"><a className="page-link" href="#">1</a></li>
+<li className="page-item"><a className="page-link" href="#">2</a></li>
+<li className="page-item"><a className="page-link" href="#">3</a></li>
+<li className="page-item">
+<a className="page-link" href="#">Next</a>
+</li>
+</ul>
+</nav>
+</div>
+</div>
+<div className="col-lg-4">
+<div className="sidebar_widget">
+<div className="single_sidebar_wiget search_form_widget">
+
+</div>
+<div className="single_sidebar_wiget">
+<div className="sidebar_tittle">
+<h3>Categories</h3>
+</div>
+<div className="single_catagory_item category">
+<ul className="list-unstyled">
+<li><a href="category.html">Culture</a> <span>(15)</span> </li>
+<li><a href="category.html">Creative Design</a> <span>(15)</span> </li>
+<li><a href="category.html">Illustration</a> <span>(15)</span> </li>
+<li><a href="category.html">Production</a> <span>(15)</span> </li>
+<li><a href="category.html">Mangement</a> <span>(15)</span> </li>
+<li><a href="category.html">Branding</a> <span>(15)</span> </li>
+</ul>
+</div>
+</div>
+<div className="single_sidebar_wiget">
+<div className="sidebar_tittle">
+<h3>Popular Feeds</h3>
+</div>
+<div className="single_catagory_post post_2 ">
+<div className="category_post_img">
+<img src="img/sidebar/sidebar_1.png" alt=""/>
+</div>
+<div className="post_text_1 pr_30">
+<a href="single-blog.html">
+<h3>Subdue lesser beast winged
+bearing meat tree one</h3>
+</a>
+<p><span> By Michal</span> / March 30</p>
+</div>
+</div>
+<div className="single_catagory_post post_2 ">
+<div className="category_post_img">
+<img src="img/sidebar/sidebar_2.png" alt=""/>
+</div>
+<div className="post_text_1 pr_30">
+<a href="single-blog.html">
+<h3>Subdue lesser beast winged
+bearing meat tree one</h3>
+</a>
+<p><span> By Michal</span> / March 30</p>
+</div>
+</div>
+<div className="single_catagory_post post_2">
+<div className="category_post_img">
+<img src="img/sidebar/sidebar_3.png" alt=""/>
+</div>
+<div className="post_text_1 pr_30">
+<a href="single-blog.html">
+<h3>Subdue lesser beast winged
+bearing meat tree one</h3>
+</a>
+<p><span> By Michal</span> / March 30</p>
+</div>
+</div>
+</div>
+<div className="single_sidebar_wiget">
+<div className="sidebar_tittle">
+<h3>Share this post</h3>
+</div>
+<div className="social_share_icon tags">
+<ul className="list-unstyled">
+<li><a href="#"><i className="ti-facebook"></i></a></li>
+<li><a href="#"><i className="ti-twitter-alt"></i></a></li>
+<li><a href="#"><i className="ti-pinterest"></i></a></li>
+<li><a href="#"><i className="ti-instagram"></i></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section className="social_connect_part">
+<div className="container-fluid">
+<div className="row">
+<div className="col-xl-12">
+<div className="social_connect">
+<div className="single-social_connect">
+<div className="social_connect_img">
+<img src="img/insta/instagram_1.png" className="" alt="blog"/>
+<div className="social_connect_overlay">
+<a href="#"><span className="ti-instagram"></span></a>
+</div>
+</div>
+</div>
+<div className="single-social_connect">
+<div className="social_connect_img">
+<img src="img/insta/instagram_2.png" className="" alt="blog"/>
+<div className="social_connect_overlay">
+<a href="#"><span className="ti-instagram"></span></a>
+</div>
+</div>
+</div>
+<div className="single-social_connect">
+<div className="social_connect_img">
+<img src="../img/insta/instagram_3.png" className="" alt="blog"/>
+<div className="social_connect_overlay">
+<a href="#"><span className="ti-instagram"></span></a>
+</div>
+</div>
+</div>
+<div className="single-social_connect">
+<div className="social_connect_img">
+<img src="img/insta/instagram_4.png" className="" alt="blog"/>
+<div className="social_connect_overlay">
+<a href="#"><span className="ti-instagram"></span></a>
+</div>
+</div>
+</div>
+<div className="single-social_connect">
+<div className="social_connect_img">
+<img src="img/insta/instagram_5.png" className="" alt="blog"/>
+<div className="social_connect_overlay">
+<a href="#"><span className="ti-instagram"></span></a>
+</div>
+</div>
+</div>
+<div className="single-social_connect">
+<div className="social_connect_img">
+<img src="img/insta/instagram_6.png" className="" alt="blog"/>
+<div className="social_connect_overlay">
+<a href="#"><span className="ti-instagram"></span></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<footer className="footer-area">
+<div className="container">
+<div className="row">
+<div className="col-xl-3 col-lg-6 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+<h4>about us</h4>
+<p>Heaven fruitful doesn't over the lesser days appear cree ping seasons so behold bea Place likeness female form. Lesser Was green image lights fowl.</p>
+</div>
+<div className="col-xl-3 col-lg-6 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+<h4>Contact Info</h4>
+<ul>
+<li><p>Address :Your address goes
+here, your demo address.</p></li>
+<li><p>Phone : +8880 44338899</p></li>
+<li><p>Email : <a href="https://preview.colorlib.com/cdn-cgi/l/email-protection" className="__cf_email__" data-cfemail="e78e898188a784888b88958b8e85c984888a">[email&#160;protected]</a></p></li>
+</ul>
+</div>
+<div className="col-xl-3 col-lg-6 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+<h4>Important Link</h4>
+<ul>
+<li><a href="#">WHMCS-bridge</a></li>
+<li><a href="#">Search Domain</a></li>
+<li><a href="#">My Account</a></li>
+<li><a href="#">Shopping Cart</a></li>
+<li><a href="#">Our Main Shop</a></li>
+</ul>
+</div>
+<div className="col-xl-3 col-lg-6 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+<h4>Newsletter</h4>
+<p>Heaven fruitful doesn't over lesser in days. Appear creeping seasons deve behold bearing days open</p>
+<div className="form-wrap" id="mc_embed_signup">
+
+
+</div>
+</div>
+</div>
+<div className="copyright_text">
+<div className="row align-items-center">
+<div className="col-lg-8">
+<div className="copyright_part">
+<p className="footer-text m-0">Copyright © 2019 All rights reserved <span className="px-2">|</span> This template is Made with <a href="https://colorlib.com/"><i className="ti-heart"></i></a> by <a href="https://colorlib.com/">Colorlib</a></p>
+</div>
+</div>
+<div className="col-lg-4 text-center text-lg-right">
+<div className="footer-social">
+<a href="#"><i className="ti-facebook"></i></a>
+<a href="#"> <i className="ti-twitter"></i> </a>
+<a href="#"><i className="ti-instagram"></i></a>
+<a href="#"><i className="ti-skype"></i></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</footer>
+        </>
+    )
+}
+
+export default Navbar
